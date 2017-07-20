d15c26a52a967d5883b0d8227c686c4c53405f9e
Viktor Petersson <vpetersson@wireload.net>
2017-07-20 10:31:56 +0100

    Fixes listen issue.

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('0.0.0.0', '5000'), app)
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
     http_server.serve_forever()
